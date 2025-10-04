04e621d86c226679a9940ce31211e4d6075a460d
Mohd Saif <iamg6102@gmail.com>
2025-10-04 14:56:55 +0530

    backing to the code

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,6 @@ app.use(
 //Routes
 import userRoute from "./routes/user.route.js"
 import itemRoute from "./routes/item.route.js"
-import googleAuthRoute from "./routes/auth.route.js";
 
 
 app.get("/", (req, res) => {
@@ -31,7 +30,6 @@ app.get("/", (req, res) => {
 //Routes Declaring
 app.use("/api/v1/users",userRoute)
 app.use("/api/v1/admin",itemRoute)
-app.use("/api/v1/auth", googleAuthRoute);
 
 
 
