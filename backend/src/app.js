7d3e8f2767426a587c21c10e75fc38780d762fb9
Mohd Saif <iamg6102@gmail.com>
2025-10-04 19:30:45 +0530

    cors adding to vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 //Cors Handling
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173","https://ecommerce-fullstack-pearl.vercel.app/"], 
     credentials: true,
   })
 );
